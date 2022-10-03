96434b9463dd5aadff31b573404dd142a76db568
Lucas Monteiro <66444506+lucas-moont@users.noreply.github.com>
2022-10-02 22:30:35 -0300

    Finished destionation buttons styling.

--- starter-code/assets/scripts/destination.js
+++ starter-code/assets/scripts/destination.js
@@ -1,4 +1,4 @@
-document.querySelectorAll('.destionationsBtn').forEach((item, index) => {
+document.querySelectorAll('.destBtn').forEach((item, index) => {
     item.addEventListener('click', (event) => {
         let destionation = event.target.getAttribute('data-destination')
         alert(destionation)
