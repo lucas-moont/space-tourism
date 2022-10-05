6a5ab6043462deba539ffd0f40ac97245b4017f7
Lucas Monteiro <66444506+lucas-moont@users.noreply.github.com>
2022-10-04 23:19:34 -0300

    Values in html now change in case mars btn is pushed.

--- starter-code/assets/scripts/destination.js
+++ starter-code/assets/scripts/destination.js
@@ -4,17 +4,19 @@ const destinations = myJson.destinations
 const destImg = document.querySelector('#destinationImg')
 const destName = document.querySelector('#destinationName')
 const destDescription = document.querySelector('#destDescription')
+const destDistance = document.querySelector('#destDistance')
+const destTravelTime = document.querySelector('#destTravelTime')
 
 document.querySelectorAll('.destBtn').forEach((item, index) => {
     item.addEventListener('click', (event) => {
-        let destionationData = event.target.getAttribute('data-destination')
-        destionationData = destionationData.toUpperCase()
-        if(destionationData == 'MARS'){
+        let destinationData = event.target.getAttribute('data-destination')
+        destinationData = destinationData.toUpperCase()
+        if(destinationData == 'MARS'){
             destName.innerHTML = destinations[1].name
             destImg.src = destinations[1].images.webp
             destDescription.innerHTML = destinations[1].description
+            destDistance.innerHTML = destinations[1].distance
+            destTravelTime.innerHTML = destinations[1].travel
         }
     })
 })
-
-console.log(destinations[1].name)
