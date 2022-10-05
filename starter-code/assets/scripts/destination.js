bb7c1245e987211aa96a649f678127ff99509486
Lucas Monteiro <66444506+lucas-moont@users.noreply.github.com>
2022-10-04 22:46:07 -0300

    Some json object values can now be read.

--- starter-code/assets/scripts/destination.js
+++ starter-code/assets/scripts/destination.js
@@ -1,12 +1,20 @@
 import myJson from '../../data.json' assert {type: 'json'}
 
-const destionations = myJson.destinations
+const destinations = myJson.destinations
+const destImg = document.querySelector('#destinationImg')
+const destName = document.querySelector('#destinationName')
+const destDescription = document.querySelector('#destDescription')
 
 document.querySelectorAll('.destBtn').forEach((item, index) => {
     item.addEventListener('click', (event) => {
-        let destionation = event.target.getAttribute('data-destination')
-        alert(destionation)
+        let destionationData = event.target.getAttribute('data-destination')
+        destionationData = destionationData.toUpperCase()
+        if(destionationData == 'MARS'){
+            destName.innerHTML = destinations[1].name
+            destImg.src = destinations[1].images.webp
+            destDescription.innerHTML = destinations[1].description
+        }
     })
 })
 
-console.log(destionations)
+console.log(destinations[1].name)
