a03395a9c422903ede888d1fd34b580875d65dfb
Lucas Monteiro <66444506+lucas-moont@users.noreply.github.com>
2022-10-04 22:26:42 -0300

    Importing json.

--- starter-code/assets/scripts/destination.js
+++ starter-code/assets/scripts/destination.js
@@ -1,6 +1,12 @@
+import myJson from '../../data.json' assert {type: 'json'}
+
+const destionations = myJson.destinations
+
 document.querySelectorAll('.destBtn').forEach((item, index) => {
     item.addEventListener('click', (event) => {
         let destionation = event.target.getAttribute('data-destination')
         alert(destionation)
     })
 })
+
+console.log(destionations)
