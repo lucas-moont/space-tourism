ac2115045bc6d180b3d682525cdb4ddff2b2dd94
Lucas Monteiro <66444506+lucas-moont@users.noreply.github.com>
2022-12-11 18:22:18 -0300

    Data changes when crew buttons are clicked.

--- starter-code/assets/scripts/crew.js
+++ starter-code/assets/scripts/crew.js
@@ -12,5 +12,15 @@ changeCrewBtns.forEach((element, index) => {
             changeCrewBtns[i].classList.remove('crewBtnActive')
         }
         element.classList.add('crewBtnActive')
+        let crewRole = document.querySelector('.crewJob')
+        let crewName = document.querySelector('.crewName')
+        let crewBio = document.querySelector('.crewDescription')
+        let crewAvatar = document.querySelector('.crewAvatar')
+
+        crewRole.innerHTML = crew[index].role.toUpperCase() 
+        crewName.innerHTML = crew[index].name.toUpperCase()
+        crewBio.innerHTML = crew[index].bio
+        crewAvatar.src = crew[index].images.webp
+
     })
 });
