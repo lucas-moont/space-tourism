a653ecb56b658609393a26888a31353e08634ee7
Lucas Monteiro <66444506+lucas-moont@users.noreply.github.com>
2023-01-01 19:23:35 -0300

    Mobile menu showing and hiding on button clicks.

---
+++ assets/scripts/menu.js
@@ -0,0 +1,11 @@
+function showMobileMenu(){
+    let menuMobile = document.querySelector('.menuMobileWrapper')
+
+    menuMobile.style.left = '0'
+}
+
+function hideMobileMenu(){
+    let menuMobile = document.querySelector('.menuMobileWrapper')
+
+    menuMobile.style.left = '100vw'
+}
